1e5abf6c4417d02fbf8ce67e8c946c6251ab7af6
atskovski <364679+atskovski@users.noreply.github.com>
2026-09-22 23:43:33 -0700

    test: make cohort v2 assertion quote agnostic

--- tests/v11-anonymity.test.js
+++ tests/v11-anonymity.test.js
@@ -32,7 +32,7 @@ test('hardened and anonymous tabs exclude extension content scripts',()=>{
 });
 
 test('maximum fingerprinting uses a cohort rather than a per-user perturbation seed',()=>{
-  assert.match(fp,/const COHORT = 'aegis-cohort-v2'/);
+  assert.match(fp,/const COHORT = ["']aegis-cohort-v2["']/);
   assert.match(fp,/STRICT \|\| ANONYMOUS \? COHORT : BASE/);
   assert.match(fp,/undef\(globalThis, 'RTCPeerConnection'\)/);
 });
